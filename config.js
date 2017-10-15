c29381f913707cc1b741043a73b85e7a5a11f623
Jay Prakash <jay.jp.prakash@gmail.com>
2017-10-15 15:44:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports={
-"database": "mongodb://root:jayprakash@ds052408.mongolab.com:52408/app",
+"database": "mongodb://localhost:27017/todo",
 "port":process.env.PORT||4000,
 "secretKey":"angelataong"	
 }
